90100635345681366ca061aac8268b48e6fbedbe
ChetanAnilMore <chetanmore733@gmail.com>
2024-06-17 12:14:11 +0530

    first commit

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "jaadui" : "https://www.youtube.com/watch?v=qNSv9I0Flac",
+    "nakhrewali" : "https://www.youtube.com/watch?v=UV7DA6-hLIo",
+    "jhumka" : "https://www.youtube.com/watch?v=VcefyDYW-VE",
+    "dance ka bhoot" : "https://www.youtube.com/watch?v=mbN9MHQ0ZTU"
+}
